e71269c258b711affd1fdf0dfb4b07b3f9c9806a
yuddomack <darr880406@gmail.com>
2020-03-09 23:59:43 +0900

    update case

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var SVTU = (function() {
+function createSVTU() {
   var author = "yuddomack";
   return {
     author: author
   };
-})();
+}
+
+// 'this' must be global obj
+if (!this.SVTU) {
+  this.SVTU = createSVTU();
+}
