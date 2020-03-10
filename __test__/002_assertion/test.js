6bd357896e710db27ea7497b3475839a7453f89b
yuddomack <darr880406@gmail.com>
2020-03-10 23:53:12 +0900

    update case

--- __test__/002_assertion/test.js
+++ __test__/002_assertion/test.js
@@ -1,5 +1,15 @@
 (function() {
-  SVTU.expect(1); // 문제없음
-  SVTU.expect(1).toBe(1); // 문제없음
-  SVTU.expect(1).toBe(2); // 문제없음
+  var num = 1;
+  var arr = [1, 2, 3];
+  var obj = { a: 1 };
+
+  SVTU.expect(num); // 문제없음
+  SVTU.expect(num).toBe(1); // 문제없음
+  SVTU.expect(num).toBe(2); // 예외발생
+
+  SVTU.expect(arr).toBe([1, 2, 3]); // 문제없음
+  SVTU.expect(arr).toBe([1, 2]); // 예외발생
+
+  SVTU.expect(obj).toBe({ a: 1 }); // 문제없음
+  SVTU.expect(obj).toBe({ a: 1, a: 2 }); // 예외발생
 })();
